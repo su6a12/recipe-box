7ba2a28d2a58672b849ed6d1f0b0f219ff411bc7
su6a12 <su6a12m3lon@gmail.com>
2017-02-21 14:32:53 -0800

    save to localStorage, display recipes and ingredients

--- MainApp.js
+++ MainApp.js
@@ -1,43 +1,11 @@
 // older version of babel was throwing 'keys not defined' error
 // switching to 5.x.x fixed the issue
-const AddRecipeModal = React.createClass({
-  close (e) {
-    e.preventDefault()
-    this.props.onClose()
-  },
-  render () {
-    if (this.props.isOpen === false) {
-      return null
-    }
-    let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%,-50%)',
-      zIndex: '100',
-      background: 'yellow',
-      padding: '20px',
-      borderRadius: '5px'
-    }
-    let modalBackground = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '99',
-      background: 'rgba(0,0,0,0.3)'
-    }
-    return (
-      <div>
-        <div style={modalStyle}>{this.props.children}</div>
-        <div style={modalBackground} onClick={e => this.close(e)}></div>
-      </div>
-    )
-  }
-});
+var recipes = localStorage['su6a12_recipes'] ? JSON.parse(localStorage['su6a12_recipes']) : []
+
+console.log(recipes)
 
 const MainApp = React.createClass({
+  // set initial flag for modal to false
   getInitialState () {
     return {
       isModalOpen: false
@@ -49,32 +17,131 @@ const MainApp = React.createClass({
   closeModal () {
     this.setState({isModalOpen: false})
   },
-  addRecipe () {
+  // handler to get recipe info from user
+  handleAddRecipe () {
     this.closeModal()
-    let title = document.getElementById('recipe-title').value
+    // if no recipe title supplied, use generic placeholder
+    let title = document.getElementById('recipe-title').value || 'No Title'
+    // separate each ingredient by a comma and remove any whitespaces around each word
     let ingredients = document.getElementById('recipe-ingredients').value.split(',').map((each) => each.trim())
-    
+    // check if recipe title already in list
+    // yes, then update ingredients list with new list
+    // no, add new recipe to recipe list
+    for(var i = 0; i < recipes.length; i++) {
+      // flag to check if we've encountered the new recipe
+      var isInList = false
+      if (recipes[i]['title'] === title) {
+        recipes[i]['ingredients'] = ingredients
+        isInList = true
+        break;
+      }
+    }
+    // if new recipe wasn't detected in recipe list
+    if (!isInList) {
+      recipes.push({title, ingredients})
+      localStorage.setItem('su6a12_recipes', JSON.stringify(recipes))
+    }
   },
   render () {
     return (
       <div>
+        <RecipeList/>
         <button id='main-add' onClick={() => this.openModal()} value='Add'>Add Recipe</button>
         <AddRecipeModal 
           isOpen={this.state.isModalOpen}
-          onClose={() => this.closeModal()}>
-          <form>
-            <div className='modal-title'>Add a Recipe</div>
+          onClose={() => this.closeModal()} 
+          onAdd={() => this.handleAddRecipe()} />
+      </div>
+    )
+  }
+})
+
+const AddRecipeModal = React.createClass({
+  closeModal (e) {
+    e.preventDefault()
+    this.props.onClose()
+  },
+  addRecipe () {
+    this.props.onAdd()
+  },
+  render () {
+    if (this.props.isOpen === false) {
+      return null
+    }
+    return (
+      <div>
+        <div className='modal-style'>
+          <div className='modal-title'>Add a Recipe</div>
             <label for='recipe-title'>Recipe</label>
             <input id='recipe-title' type='text' placeholder='Recipe Name' size='50' />
             <label for='recipe-ingredients'>Ingredients</label>
             <textarea id='recipe-ingredients' type='textarea' placeholder='Separate, By, Commas' rows='10' cols='50'/>
             <button onClick={() => this.addRecipe()}>Add Recipe</button>
-            <button onClick={() => this.closeModal()}>Close</button>
-          </form>
-        </AddRecipeModal>
+            <button onClick={e => this.closeModal(e)}>Close</button>
+        </div>
+        <div className='modal-background' onClick={e => this.closeModal(e)}></div>
       </div>
     )
   }
-});
+})
 
-ReactDOM.render(<MainApp />, document.getElementById('app'));
+const RecipeList = React.createClass({
+  render () {
+    let recipeList = recipes.map((recipe, index) => {
+      return (
+        <Recipe 
+          recipe={recipe}
+          key={index} />
+      )
+    })
+    return (
+      <ul>
+        {recipeList}
+      </ul>
+    )
+  }
+})
+
+const Recipe = React.createClass({
+  getInitialState () {
+    return {
+      isChangeModalOpen: false
+    }
+  },
+  openChangeModal () {
+    this.setState({isChangeModalOpen: true})
+  },
+  closeChangeModal () {
+    this.setState({isChangeModalOpen: false})
+  },
+  handleChangeModal () {
+    return
+  },
+  render () {
+    let ingredientsList = this.props.recipe.ingredients.map((ingredient) => {
+      return (
+        <div className='ingredient'>{ingredient}</div>
+      )
+    })
+    return (
+      <div>
+        <li onClick={() => this.handleChangeModal()}>
+          {this.props.recipe.title}
+          {ingredientsList}
+        </li>
+        <ChangeRecipeModal 
+          isOpen={this.state.isChangeModalOpen}
+          onClose={() => this.closeChangeModal()}
+          onOpen={() => this.openChangeModal()} />
+      </div>
+    )
+  }
+})
+
+const ChangeRecipeModal = React.createClass({
+  render () {
+    return <div></div>
+  }
+})
+
+ReactDOM.render(<MainApp />, document.getElementById('mainApp'));
